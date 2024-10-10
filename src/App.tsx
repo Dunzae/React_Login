25da1fe2b954540910760ea598056aae46569d23
dunzae <dunzae99@gmail.com>
2024-10-11 07:27:07 +0900

    @Feat react-router-dom 패키지 추가 및 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
+
+
 const App = () => {
-  return <div>Hello, world!</div>;
+  return <RouterProvider router={router} />;
 };
 export default App;
