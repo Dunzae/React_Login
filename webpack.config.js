7fbcce7bc1f4698305d9c14518ce8d18e57fdb2d
dunzae <dunzae99@gmail.com>
2024-10-10 20:30:23 +0900

    @Feat Babel과 Webpack을 이용한 리액트 앱 만들기

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js',
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            { test: /\.tsx?$/, loader: "ts-loader" },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { test: /\.js$/, loader: "source-map-loader" },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            
+        ],
+    },
+    resolve: {
+        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        hot: true,
+        open: true,
+    },
+};
