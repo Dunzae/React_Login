1cda9bfb66e319ffa68d4fc148aa969dfde8621b
dunzae <dunzae99@gmail.com>
2024-10-12 15:51:52 +0900

    @Style normalize.css와 전역 스타일 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from "react-router-dom";
+import "normalize.css"
+import "./global.css";
 import router from "./Router";
-
 
 const App = () => {
   return <RouterProvider router={router} />;
