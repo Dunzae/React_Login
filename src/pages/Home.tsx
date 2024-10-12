77222307b847e7ff6a3e347cb9ac3948b591390f
dunzae <dunzae99@gmail.com>
2024-10-12 15:52:45 +0900

    @Style HomePage에 배경화면 설정

---
+++ src/pages/Home.tsx
@@ -0,0 +1,20 @@
+function HomePage() {
+    return <div className="w-full h-full bg-[url('assets/images/background.png')]">
+        {/* <form>
+            <p>로그인</p>
+            <div>
+                <label>아이디 : </label>
+                <input className="border-2 border-black"/>
+            </div>
+            <div>
+                <label>비밀번호 : </label>
+                <input className="border-2 border-black"/>
+            </div>
+            <div>
+                <button type="submit" className="border border-black">로그인 버튼</button>
+            </div>
+        </form> */}
+    </div>
+}
+
+export default HomePage;
