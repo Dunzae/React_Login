6dd989c6fefe072927869120f3c1c66e1b40cdb8
dunzae <dunzae99@gmail.com>
2024-10-13 11:43:48 +0900

    @Design 로그인 시 에러문을 출력하는 디자인 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,14 @@ import { SubmitHandler, useForm } from "react-hook-form";
 import { useCallback } from "react";
 
 type Inputs = {
-    id : string;
-    password : string;
-    remember : boolean;
+    id: string;
+    password: string;
+    remember: boolean;
 }
 
 function HomePage() {
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit : SubmitHandler<Inputs> = useCallback(data => {
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = useCallback(data => {
         console.log(data);
     }, [])
 
@@ -24,9 +24,9 @@ function HomePage() {
             <div className="flex w-full px-[20px] py-[10px] overflow-hidden">
                 <div className="flex px-[10px] py-[5px] justify-between items-center border-2 border-white border-opacity-50 rounded-2xl w-full h-full">
                     <div className="flex w-full h-full mr-2">
-                        <input className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none" 
-                        placeholder="Username" 
-                        {...register("id", {required : true})} />
+                        <input className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none"
+                            placeholder="Username"
+                            {...register("id", { required: true })} />
                     </div>
                     <div className="flex h-full text-white">
                         <FaRegUser />
@@ -36,23 +36,24 @@ function HomePage() {
             <div className="flex w-full px-[20px] py-[10px] overflow-hidden">
                 <div className="flex px-[10px] py-[5px] justify-between items-center border-2 border-white border-opacity-50 rounded-2xl w-full h-full">
                     <div className="flex w-full h-full mr-2 ">
-                        <input type="password" className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none" 
-                        placeholder="Password" 
-                        {...register("password", {required : true})} />
+                        <input type="password" className="w-full h-full bg-transparent text-white placeholder:text-white outline-none border-none"
+                            placeholder="Password"
+                            {...register("password", { required: true })} />
                     </div>
                     <div className="flex h-full text-white">
                         <FaLock />
                     </div>
                 </div>
             </div>
-            <div className="flex justify-between w-full px-[20px] py-[10px] mb-[10px] ">
+            
+            <div className="flex justify-between w-full px-[20px] py-[10px]">
                 <div className="flex justify-between items-center w-full">
                     <div className="flex ">
                         <div className="mr-1">
-                            <input id="remember"  
-                            type="checkbox" 
-                            className="cursor-pointer" 
-                            {...register("remember")}/>
+                            <input id="remember"
+                                type="checkbox"
+                                className="cursor-pointer"
+                                {...register("remember")} />
                         </div>
                         <span className="text-white">
                             <label htmlFor="remember" className="cursor-pointer">Remember Me</label>
@@ -63,15 +64,20 @@ function HomePage() {
                     </div>
                 </div>
             </div>
-            <div className="flex w-full px-[20px] mb-[20px]">
+            <div className="flex w-full px-[20px] mb-[10px]">
                 <div className="flex py-2 justify-center w-full rounded-3xl bg-white ">
                     <button type="submit" className="w-full h-full">Login</button>
                 </div>
             </div>
-            <div className="flex w-full px-[20px] pb-[10px] text-white justify-center">
-                <div>
+            <div className="flex w-full">
+                <div className='flex w-full px-[20px] pb-[10px] text-white justify-center'>
                     <span className="mr-2">Don't have account?</span>
                     <button type="button" className="border-b-2 border-transparent hover:border-white">Register</button>
+                </div>
+            </div>
+            <div className="w-full mb-[10px]">
+                <div className="text-red-400 flex justify-center w-full py-[10px]">
+                    사용자가 존재하지 않습니다
                 </div>
             </div>
         </form>
