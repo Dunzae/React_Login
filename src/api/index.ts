c7c692760d4c840f5e75d750118b799c854e4a70
dunzae <dunzae99@gmail.com>
2024-10-13 14:45:43 +0900

    @Feat axios 모듈에 있는 AxiosInstance에 커스텀 함수 추가 및 타입 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,48 @@
-import axios from "axios";
+import axios, { AxiosInstance, isAxiosError } from "axios";
 
-export default axios.create({
+const axiosInstance : AxiosInstance = axios.create({
     baseURL : process.env.SERVER_URL,
     headers : {
         'Content-Type' : "application/x-www-form-urlencoded"
     }
 })
+
+axiosInstance.getApi = async (url : string, data : any) => {
+    try {
+        const result = await axiosInstance.get(url, data)
+        return result.data;
+    } catch(e) {
+        if (isAxiosError(e)) {
+            if(e.response === undefined) {
+                return {
+                    error: "The server is down",
+                }
+            }
+            return e.response?.data;
+        }
+        return {
+            error: "Unknown error",
+        }
+    }
+}
+
+axiosInstance.postApi = async (url : string, data : any) => {
+    try {
+        const result = await axiosInstance.post(url, data)
+        return result.data;
+    } catch(e) {
+        if (isAxiosError(e)) {
+            if(e.response === undefined) {
+                return {
+                    error: "The server is down",
+                }
+            }
+            return e.response?.data;
+        }
+        return {
+            error: "Unknown error",
+        }
+    }
+}
+
+export default axiosInstance;
