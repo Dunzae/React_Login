17fdf29a8c63d584e9b5a672f5c2058bcacc57d6
dunzae <dunzae99@gmail.com>
2024-10-16 18:37:01 +0900

    @Design 메뉴바 퍼블리싱 완료

---
+++ src/components/Menubar.tsx
@@ -0,0 +1,35 @@
+import {
+    IoHome, IoHomeOutline, IoBookmark, IoBookmarkOutline, IoDocumentTextOutline, IoDocumentTextSharp,
+    IoPerson, IoPersonOutline
+} from "react-icons/io5";
+import { IoMdNotifications, IoMdNotificationsOutline } from "react-icons/io";
+
+interface IMenubarComponent {
+    currentPath: string
+}
+
+function MenubarComponent({
+    currentPath
+}: IMenubarComponent) {
+    return (
+        <ul className="flex flex-row lg:flex lg:flex-col lg:items-center lg:py-5">
+            <li className="p-4 flex flex-grow justify-center lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center">
+                {currentPath === "/" ? <IoHome size={30} /> : <IoHomeOutline size={30} />}
+            </li>
+            <li className="p-4 flex flex-grow justify-center lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center">
+                {currentPath === "/notification" ? <IoMdNotifications size={30} /> : <IoMdNotificationsOutline size={30} />}
+            </li>
+            <li className="p-4 flex flex-grow justify-center lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center">
+                {currentPath === "/bookmark" ? <IoBookmark size={30} /> : <IoBookmarkOutline size={30} />}
+            </li>
+            <li className="p-4 flex flex-grow justify-center lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center">
+                {currentPath === "/posts" ? <IoDocumentTextSharp size={30} /> : <IoDocumentTextOutline size={30} />}
+            </li>
+            <li className="p-4 flex flex-grow justify-center lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center">
+                {currentPath === "/profile" ? <IoPerson size={30} /> : <IoPersonOutline size={30} />}
+            </li>
+        </ul>
+    )
+}
+
+export default MenubarComponent;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,36 @@
+import TitleComponent from "@components/Title";
+import ProfileComponent from "@components/Profile";
+import MenubarComponent from "@components/Menubar";
+
+
 function HomePage() {
+    const currentPath: string = "/";
+
     return (
         <div className="h-full lg:flex lg:flex-row">
-            <div className="h-[100px] fixed bg-white lg:w-[300px] lg:h-full lg:flex lg:flex-col">
-                <div className="h-full border">
-                    <div className="h-full flex">
-                        프로필
-                        {/* <Profile /> */}
-                    </div>
-                </div>
-                <div className="w-[100px] h-full absolute border top-0 right-0 lg:w-full lg:static">
-                    <div className="h-full flex">
-                        카테고리
-                        {/* <Category /> */}
+            <div className="h-[100px] fixed lg:w-[400px] lg:h-full lg:flex lg:flex-col lg:bg-[url('assets/images/sideBarBackground.jpg')] lg:bg-cover lg:bg-origin lg:bg-[-80px_0%] lg:justify-center">
+                <div className="h-auto">
+                    <div className="h-full flex flex-col">
+                        <div className="text-black flex justify-center lg:w-full lg:text-white lg:flex lg:justify-center lg:mb-2">
+                            <TitleComponent />
+                        </div>
+                        <div className="hidden lg:flex">
+                            <ProfileComponent
+                                nameColor="white"
+                                tagColor="gray"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="h-[100px] lg:w-[300px] lg:h-full flex-shrink-0" />
-            <div className="flex-shrink-0">
+            <div className="h-[100px] lg:w-[400px] lg:h-full flex-shrink-0" />
+            {/* 사이드 메뉴바 */}
+            <div className="fixed bottom-0 w-full lg:static lg:w-[100px] lg:h-full lg:flex lg:items-center border border-[#E6E6E6] ">
+                <MenubarComponent currentPath="/" />
+                {/* <NavigationMenu /> */}
+            </div>
+            <div className="">
+
                 Body
                 {/* <PostList>
                     <Post/>
