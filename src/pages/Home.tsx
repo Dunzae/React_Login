fa533e84c1e855870f10ae4aa84f656460f90554
dunzae <dunzae99@gmail.com>
2024-10-16 11:24:19 +0900

    @Design 홈 기본 레이아웃 완성

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,29 @@
 function HomePage() {
     return (
-        <div className="">
-            <div className="h-[100px] fixed bg-white">
-                <div className="h-full p-3 border">
-                    <div className="h-full flex justify-center items-center">프로필</div>
+        <div className="h-full lg:flex lg:flex-row">
+            <div className="h-[100px] fixed bg-white lg:w-[300px] lg:h-full lg:flex lg:flex-col">
+                <div className="h-full border">
+                    <div className="h-full flex">
+                        프로필
+                        {/* <Profile /> */}
+                    </div>
                 </div>
-                <div className="h-full absolute border top-0 right-0">
-                    <div className="h-full flex justify-center items-center">
+                <div className="w-[100px] h-full absolute border top-0 right-0 lg:w-full lg:static">
+                    <div className="h-full flex">
                         카테고리
+                        {/* <Category /> */}
                     </div>
                 </div>
             </div>
-            <div className=" h-[100px]" />
+            <div className="h-[100px] lg:w-[300px] lg:h-full flex-shrink-0" />
+            <div className="flex-shrink-0">
+                Body
+                {/* <PostList>
+                    <Post/>
+                    <Post />
+                    <Post />
+                </PostList> */}
+            </div>
         </div>
     )
 }
