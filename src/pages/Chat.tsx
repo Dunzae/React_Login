1d2eb71da27d49e4d30cc2b044e32da0151d917f
dunzae <dunzae99@gmail.com>
2024-10-19 08:11:16 +0900

    @Design 채팅페이지의 기본 레이아웃 구조 완성

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -3,8 +3,17 @@ import LayoutComponent from "@components/Layout";
 function ChatPage() {
     return (
         <LayoutComponent>
-            <div className="px-[10px] py-[10px] min-h-dvh bg-[#f2f2f2] lg:px-[40px] lg:py-[40px]">
-                
+            <div className="min-h-dvh bg-[#f2f2f2]">
+                <div className='flex h-dvh overflow-hidden lg:min-w-[800px]'>
+                    <div className="flex-grow lg:w-[40%] lg:basis-auto lg:h-full border border-black">
+                        <div>검색바</div>
+                        <div>채팅한 사람들 목록</div>
+                    </div>
+                    <div className="flex-grow w-0 lg:w-[60%] lg:basis-auto lg:h-full border border-black">
+                        <div>채팅하고 있는 사람</div>
+                        <div>채팅 컴포넌트</div>
+                    </div>
+                </div>
             </div>
         </LayoutComponent>
     )
