9e5eecb95257027d0896c0bc2806486fa74c9800
dunzae <dunzae99@gmail.com>
2024-10-18 10:09:55 +0900

    @Design 메뉴바 컴포넌트 자체가 전체 보더를 갖도록 수정

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -15,23 +15,26 @@ function MenubarComponent({
     const navigate = useNavigate();
 
     return (
-        <ul className="w-full flex flex-row flex-grow-0 bg-white lg:flex lg:flex-col lg:items-center lg:py-5 lg:border-r lg:border-r-[#E6E6E6]">
-            <li onClick={() => navigate("/")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
-                {currentPath === "/" ? <IoHome size={30} /> : <IoHomeOutline size={30} />}
-            </li>
-            <li className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
-                {currentPath === "/notification" ? <IoMdNotifications size={30} /> : <IoMdNotificationsOutline size={30} />}
-            </li>
-            <li onClick={() => navigate("/bookmark")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
-                {currentPath === "/bookmark" ? <IoBookmark size={30} /> : <IoBookmarkOutline size={30} />}
-            </li>
-            <li onClick={() => navigate("/post")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
-                {currentPath === "/post" ? <IoDocumentTextSharp size={30} /> : <IoDocumentTextOutline size={30} />}
-            </li>
-            <li onClick={() => navigate("/profile")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
-                {currentPath === "/profile" ? <IoPerson size={30} /> : <IoPersonOutline size={30} />}
-            </li>
-        </ul>
+        <div className="border-t border-t-[#E6E6E6] lg:h-full lg:border-t-0 lg:border-r lg:border-r-[#E6E6E6] lg:flex lg:items-center">
+            <ul className="w-full flex flex-row flex-grow-0 bg-white lg:flex lg:flex-col lg:items-center lg:py-5">
+                <li onClick={() => navigate("/")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
+                    {currentPath === "/" ? <IoHome size={30} /> : <IoHomeOutline size={30} />}
+                </li>
+                <li className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
+                    {currentPath === "/notification" ? <IoMdNotifications size={30} /> : <IoMdNotificationsOutline size={30} />}
+                </li>
+                <li onClick={() => navigate("/bookmark")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
+                    {currentPath === "/bookmark" ? <IoBookmark size={30} /> : <IoBookmarkOutline size={30} />}
+                </li>
+                <li onClick={() => navigate("/post")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
+                    {currentPath === "/post" ? <IoDocumentTextSharp size={30} /> : <IoDocumentTextOutline size={30} />}
+                </li>
+                <li onClick={() => navigate("/profile")} className="p-4 flex flex-grow justify-center lg:flex-grow-0 lg:mx-2 lg:my-8 lg:p-2 lg:w-[60px] lg:h-[30px] lg:flex lg:justify-center lg:items-center lg:cursor-pointer">
+                    {currentPath === "/profile" ? <IoPerson size={30} /> : <IoPersonOutline size={30} />}
+                </li>
+            </ul>
+        </div>
+
     )
 }
 
